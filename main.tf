7e335dcbd965d700b77f24f983f8cc86cca0cc9b
rrsahoo123 <ratiranjansh@gmail.com>
2023-04-13 06:54:26 +0000

    added tf backend

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 #setting-up provider
+terraform {
+  backend "s3" {
+    bucket = "mmyada1"
+    key    = "terraform-state-file/non-prod/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "rati-tf-dy-db-locking"
+  }
+}
+
 
 provider "aws" {
     region = "us-east-1"  
