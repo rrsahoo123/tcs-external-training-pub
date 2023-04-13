7a55d745e4e7eb03711b3181d4d59f824a51c498
rrsahoo123 <ratiranjansh@gmail.com>
2023-04-13 05:25:02 +0000

    removed access and secret keys

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@
 
 provider "aws" {
     region = "us-east-1"  
-    access_key = "AKIA4MFLODBGD2DGTQ5O"
-    secret_key = "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"
+
 }
 
 # write the ec2 instance config 
